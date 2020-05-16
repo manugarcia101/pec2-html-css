90d237743004df54050b81f91a8ff65fd956f546
Manugarcia101 <manugarcial96@gmail.com>
2020-05-16 11:33:47 +0200

    reconfigurando imagemin parcel plugin

--- webp.js
+++ webp.js
@@ -1,21 +1,3 @@
-// var imagemin = require("imagemin");
-// var webp = require("imagemin-webp");
-// var outputFolder = "./app/images";
-// var PNGImages = "./app/images/*.png";
-// var JPEGImages = "./app/images/*.jpg";
-
-// imagemin([PNGImages], outputFolder, {
-//   plugins: [webp({
-//       lossless: true 
-//   })]
-// });
-
-// imagemin([JPEGImages], outputFolder, {
-//   plugins: [webp({
-//     quality: 65 
-//   })]
-// });
-
 const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
  
